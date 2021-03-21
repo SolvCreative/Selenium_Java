b137c4c68e84e295aff42659e8a9f2870267b473
Jenn <solvcreative@gmail.com>
2021-03-21 18:50:13 -0500

    Fixed typo

--- src/SolvCreative/FirstScript.java
+++ src/SolvCreative/FirstScript.java
@@ -6,14 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstScript {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException{
 		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.get("httos://login.yahoo.com/");
+		driver.get("https://login.yahoo.com/");
 		driver.findElement(By.xpath("//input[@id='login-username']")).sendKeys("jenniferhodges2@yahoo.com");
 		Thread.sleep(10000);
 		driver.findElement(By.xpath("//input[@id='login-signin']")).click();
